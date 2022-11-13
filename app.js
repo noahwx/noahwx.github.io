c31f64967db1ed91b263d69d08437aeadb2a3d4b
Noah Fontenette <47571584+noahwx@users.noreply.github.com>
2022-11-13 14:28:24 -0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    var element = document.getElementById("menu");
-    if (element.style.visibility == "visible") {
-        element.style.visibility = "hidden";
-    } else {
-        element.style.visibility = "visible";
-    }
-}
