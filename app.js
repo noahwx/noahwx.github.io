a99d414100c7b8b699e28d06f690c8c1b29c248d
Noah Fontenette <noahfontenette@outlook.com>
2022-11-19 15:58:20 -0600

    Verison 0.0.033m

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ function toggleNav() {
         element.style.visibility = "visible";
     }
 }
+
+function togglePost() {
+    var element = document.getElementById("m-post");
+    var postBtn = document.getElementById("mobile-post-btn");
+    var postBtnC = document.getElementById("mobile-post-btn-c");
+
+    if (element.style.visibility == "visible") {
+        element.style.visibility = "hidden";
+    } else {
+        element.style.visibility = "visible";
+    }
+
+    if (postBtn.style.visibility == "hidden") {
+        postBtn.style.visibility = "visible";
+    } else {
+        postBtn.style.visibility = "hidden";
+    }
+
+    if (postBtnC.style.visibility == "visible") {
+        postBtnC.style.visibility = "hidden";
+    } else {
+        postBtnC.style.visibility = "visible";
+    }
+}
