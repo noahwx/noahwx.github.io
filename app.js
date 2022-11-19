8a2460cd0e71b9c2faeef77d225a86ab9944d6b3
Noah Fontenette <noahfontenette@outlook.com>
2022-11-19 11:33:15 -0600

    Verison 0.0.016m

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var element = document.getElementById("menu");
+    if (element.style.visibility == "visible") {
+        element.style.visibility = "hidden";
+    } else {
+        element.style.visibility = "visible";
+    }
+}
